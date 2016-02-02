add25dcf086758f9a239857bee1f875b48a8075d
Kirill Buga <tztimezero@yandex.ru>
2016-02-02 22:10:51 +0200

    Play with chart colors and styles

--- public/src/stats/countries/stats.countries.decorator.js
+++ public/src/stats/countries/stats.countries.decorator.js
@@ -1,26 +1,27 @@
 define(['chartjs'], function(Chart) {
 	var barChartDecorator = function (node, countriesData) {
-		var ctx = node.getContext("2d");
+		var ctx = node.getContext('2d');
 		var data = {
 			labels: countriesData.map(function(item) {
 				return item.country;
 			}),
 			datasets: [
 				{
-					label: "Countries Stats",
-					fillColor: "rgba(220,220,220,0.2)",
-					strokeColor: "rgba(220,220,220,1)",
-					pointColor: "rgba(220,220,220,1)",
-					pointStrokeColor: "#fff",
-					pointHighlightFill: "#fff",
-					pointHighlightStroke: "rgba(220,220,220,1)",
+					label: 'Countries Stats',
+					fillColor: '#B8B8B8',
+					strokeColor: '#B8B8B8',
+					pointHighlightStroke: 'rgba(220,220,220,1)',
 					data: countriesData.map(function(item) {
 						return item.value;
 					})
 				}
 			]
 		};
-		var barChart = new Chart(ctx).Bar(data, {});
+		var barChart = new Chart(ctx).Bar(data, {
+			barDatasetSpacing : 5,
+			barValueSpacing : 40,
+			scaleShowHorizontalLines: false
+		});
 		return {
 			teardown: function () {
 				barChart.destroy();
--- public/src/stats/visitors/stats.visitors.decorator.js
+++ public/src/stats/visitors/stats.visitors.decorator.js
@@ -1,33 +1,46 @@
 define(['chartjs'], function(Chart) {
+	var monthArray = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT','NOV', 'DEC'];
 	var lineChartDecorator = function (node, visitorsData) {
-		var ctx = node.getContext("2d");
+		var ctx = node.getContext('2d');
 		var data = {
 			labels: visitorsData.map(function(item) {
-				var date = new Date(item.timestamp);
-				return date.getDate() + ' ' + (date.getMonth() + 1) + ' ' + date.getFullYear();
+				return formatDate(item.timestamp);
 			}),
 			datasets: [
 				{
-					label: "Visitors Stats",
-					fillColor: "rgba(220,220,220,0.2)",
-					strokeColor: "rgba(220,220,220,1)",
-					pointColor: "rgba(220,220,220,1)",
-					pointStrokeColor: "#fff",
-					pointHighlightFill: "#fff",
-					pointHighlightStroke: "rgba(220,220,220,1)",
+					label: 'Visitors Stats',
+					fillColor: 'transparent',
+					strokeColor: '#8EACF8',
+					pointColor: '#8EACF8',
+					pointStrokeColor: '#fff',
+					pointHighlightFill: '#fff',
+					pointHighlightStroke: 'rgba(220,220,220,1)',
 					data: visitorsData.map(function(item) {
 						return item.value;
 					})
 				}
 			]
 		};
-		var lineChart = new Chart(ctx).Line(data, {});
+		var lineChart = new Chart(ctx).Line(data, {
+			bezierCurve: false,
+			scaleShowVerticalLines: false,
+			pointHitDetectionRadius: 5
+		});
 		return {
 			teardown: function () {
 				lineChart.destroy();
 			}
 		};
 	};
+
+	function formatDate(timestamp) {
+		var date = new Date(timestamp);
+		return date.getDate() + ' ' + getReadableMonth(date.getMonth()) + ' ' + date.getFullYear();
+	}
+
+	function getReadableMonth(monthIndex) {
+		return monthArray[monthIndex];
+	}
 
 	return lineChartDecorator;
 });
