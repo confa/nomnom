869bdcff04647644c2d4a7430795bf0c7618bc8f
Kirill Buga <tztimezero@yandex.ru>
2016-02-02 21:07:57 +0200

    Amend navbar so it disables the button while deck is not selected

--- public/src/app.router.js
+++ public/src/app.router.js
@@ -1,11 +1,16 @@
-define(['backbone', 'deckPicker/deckPicker', 'slideViewer/slideViewer'], 
-	function(Backbone, DeckPicker, SlideViewer) {
+define(['backbone', 'dashboard/dashboard', 'deckPicker/deckPicker', 'slideViewer/slideViewer'], 
+	function(Backbone, Dashboard, DeckPicker, SlideViewer) {
 	var Router = Backbone.Router.extend({
 		currentView_: null,
 		routes: {
 			'':                     'root',
 			'slide/:name(/:index)': 'slide',
 			'stats/:name':          'stats'
+		},
+		initialize: function () {
+			this.on('route', function(name) {
+				Dashboard.setCurrentRoute(name);
+			});
 		},
 		root: function() {
 			this.currentView_ = new DeckPicker({
@@ -19,8 +24,10 @@ define(['backbone', 'deckPicker/deckPicker', 'slideViewer/slideViewer'],
 				currentSlide: index
 			});
 			this.currentView_.set('deckName', name);
+			Dashboard.setCurrentDeck(name);
 		},
-		stats: function(name) {	
+		stats: function(name) {
+			Dashboard.setCurrentDeck(name);
 		}
 	});
 	return Router;
